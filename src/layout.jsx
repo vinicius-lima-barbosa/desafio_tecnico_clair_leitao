985336e62778c864e464ad809e10ba99e44cd9c4
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-05-19 20:31:18 -0300

    feat: Implement routing with AppRoutes and add Home and Layout components

---
+++ src/layout.jsx
@@ -0,0 +1,10 @@
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <h1>Teste</h1>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import AppRoutes from "./app-routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AppRoutes />
   </StrictMode>
 );
