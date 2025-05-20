bfc5ec7f21b2b604ca25d9a4c83fc8dc5c6ed338
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-05-20 18:29:56 -0300

    feat: add movie detail link in movie-card

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -7,22 +7,30 @@ import {
   MovieMeta,
   MovieMetaRating,
 } from "../styles/styled";
+import { Link } from "react-router-dom";
 
 const MOVIE_POSTER = import.meta.env.VITE_TMDB_API_MOVIE_IMAGE;
 
 const MovieCard = ({ movie }) => {
   return (
     <Col xs={6} sm={4} md={3} lg={2}>
-      <MovieCardStyled>
-        <MovieImage src={MOVIE_POSTER + movie.poster_path} alt={movie.title} />
-        <MovieInfo>
-          <MovieTitle>{movie.title}</MovieTitle>
-          <MovieMeta>
-            <span>{movie.release_date?.slice(0, 4) || "N/A"}</span>
-            <MovieMetaRating>{movie.vote_average?.toFixed(1)}</MovieMetaRating>
-          </MovieMeta>
-        </MovieInfo>
-      </MovieCardStyled>
+      <Link to={`/movie/${movie.id}`}>
+        <MovieCardStyled>
+          <MovieImage
+            src={MOVIE_POSTER + movie.poster_path}
+            alt={movie.title}
+          />
+          <MovieInfo>
+            <MovieTitle>{movie.title}</MovieTitle>
+            <MovieMeta>
+              <span>{movie.release_date?.slice(0, 4) || "N/A"}</span>
+              <MovieMetaRating>
+                {movie.vote_average?.toFixed(1)}
+              </MovieMetaRating>
+            </MovieMeta>
+          </MovieInfo>
+        </MovieCardStyled>
+      </Link>
     </Col>
   );
 };
