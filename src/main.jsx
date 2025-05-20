a696501996d38d02428e48792fcbd6363b5e41b6
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-05-19 21:20:02 -0300

    fix: minors

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import AppRoutes from "./app-routes.jsx";
+import AppRoutes from "./app-routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
